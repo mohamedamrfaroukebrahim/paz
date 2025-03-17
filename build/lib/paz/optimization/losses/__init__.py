99477861dc16d699cb2ef8f17804af52c8a6f9c1
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-03-17 10:34:46 +0100

    Remove build directory

--- build/lib/paz/optimization/losses/__init__.py
+++
@@ -1,5 +0,0 @@
-from .multi_box_loss import MultiBoxLoss
-from .keypointnet_loss import KeypointNetLoss
-from .segmentation import DiceLoss
-from .segmentation import FocalLoss
-from .segmentation import JaccardLoss
