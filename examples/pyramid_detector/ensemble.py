42b3d00156909c4491e201857c96b1acd54c85d0
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-05-10 05:32:47 +0200

    Fix ensemble prediction

--- examples/pyramid_detector/ensemble.py
+++ examples/pyramid_detector/ensemble.py
@@ -1,3 +1,6 @@
+import os
+
+os.environ["KERAS_BACKEND"] = "jax"
 from glob import glob
 import jax
 import jax.numpy as jp
@@ -9,7 +12,7 @@ def load(wildcard):
 
 
 def predict(models, image, activation=jax.nn.sigmoid):
-    return jp.array([activation(model.predict(image)) for model in models])
+    return jp.array([activation(jp.squeeze(model(image))) for model in models])
 
 
 def compute_entropy(predictions, num_bins=10):
@@ -17,12 +20,25 @@ def compute_entropy(predictions, num_bins=10):
     probability predictions using histogram binning.
     """
 
-    def p_log_p(p, epsilon=1e-12):
+    def p_log2_p(p, epsilon=1e-12):
         log_p = jp.log2(p + epsilon)
         return p * log_p
 
     counts, _ = jp.histogram(predictions, bins=num_bins, range=(0.0, 1.0))
     probability_mass = counts / jp.sum(counts)
-    # remove vmap
-    entropy = -jp.sum(jax.vmap(p_log_p)(probability_mass))
+    entropy = -jp.sum(p_log2_p(probability_mass))
+
     return entropy
+
+
+if __name__ == "__main__":
+    import deepfish
+    from pipeline import batch
+    from generator import Generator
+
+    models = load("experiments/*_ensemble_*/simple.keras")
+    train_images, train_labels = deepfish.load("Deepfish/", "train")
+    key = jax.random.PRNGKey(777)
+    train_generator = Generator(key, train_images, train_labels, batch)
+    image = train_generator.__getitem__(0)[0][0:1]
+    predictions = predict(models, image)
