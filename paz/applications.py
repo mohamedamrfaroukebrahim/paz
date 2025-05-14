3ced50b1a4948a089a7f61db3f73107ad4aa854d
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-05-14 09:26:00 +0200

    Fix bug with import

--- paz/applications.py
+++ paz/applications.py
@@ -1,3 +1,3 @@
 from paz.models.classification.xception import MiniXceptionFER
-from paz.models.detection.haar_cascade import HaarCascadeFaceDetector
+from paz.models.detection.haar_cascade import HaarCascadeFrontalFaceDetector
 from paz.models.detection.haar_cascade import HaarCascadeEyeDetector
