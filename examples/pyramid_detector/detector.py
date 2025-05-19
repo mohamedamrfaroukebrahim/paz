afb4ac71bbb3e997286acef485010a3facc2e600
Octavio Arriaga <arriaga.camargo@gmail.com>
2025-05-19 15:37:28 +0200

    Add batch predictor for jax only inference

--- examples/pyramid_detector/detector.py
+++ examples/pyramid_detector/detector.py
@@ -1,6 +1,8 @@
 import os
 
 os.environ["KERAS_BACKEND"] = "jax"
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = ".95"
+import math
 import jax
 import jax.numpy as jp
 
@@ -9,21 +11,45 @@ from patch import boxes_patch
 import ensemble
 import paz
 
+
+def predict_in_batches(x, model, batch_size=100):
+    num_samples = x.shape[0]
+    num_batches = math.ceil(num_samples / batch_size)
+
+    predictions = []
+    for batch_arg in range(num_batches):
+        intro_index = batch_arg * batch_size
+        outro_index = min((batch_arg + 1) * batch_size, num_samples)
+        batch = x[intro_index:outro_index]
+        minibatch_predictions = model(batch, training=False)
+        predictions.append(minibatch_predictions)
+    return jax.nn.sigmoid(jp.concatenate(predictions, axis=0))
+
+
 H = 480
 W = 640
+patch_size = (256, 256)
 patch_size = (128, 128)
 strides = (32, 32)
-score_threshold = 0.90
+score_threshold = 0.35  # 90
 entropy_threshold = 0.75
 iou_threshold = 0.1
 num_models = 20
 scales = [1.0]
 padding = "same"
+model = "xception"
 model = "simple"
 
 valid_images, valid_labels = load("Deepfish/", "validation")
 models = ensemble.load(f"experiments/*_ensemble_*/{model}.keras", num_models)
-predict = jax.jit(paz.partial(ensemble.predict, models))
+print(f"Loaded {len(models)} models")
+
+
+if len(models) == 1:
+    predict = jax.jit(paz.lock(predict_in_batches, models[0], 32))
+else:
+    predict = jax.jit(paz.partial(ensemble.predict, models))
+
 patch = jax.jit(paz.lock(paz.image.patch, patch_size, strides, padding))
 
 
@@ -39,12 +65,12 @@ for sample_arg in range(0, len(valid_images), 100):
             *paz.image.get_size(pyramid), patch_size, strides, padding
         )
         boxes = boxes.reshape(-1, 4)
+        # score = jp.squeeze(predict(patches), axis=-1)
         model_scores = predict(patches)
         model_scores = jp.squeeze(model_scores, axis=-1)
         score = jp.mean(model_scores, axis=0)
         is_positive = score > score_threshold
         entropies = jax.vmap(ensemble.compute_entropy, in_axes=1)(model_scores)
-        print(entropies)
         i_am_certain = entropies < entropy_threshold
         is_positive = is_positive & i_am_certain
         pyramid_boxes.append(paz.boxes.scale(boxes[is_positive], scale, scale))
